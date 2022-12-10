1d543e4d4c78886168aaf616c213e78a2fd2f7bc
purvesh0110 <97841283+purvesh0110@users.noreply.github.com>
2022-12-10 15:36:14 +0530

    Update practical_1.cpp

--- practical_1.cpp
+++ practical_1.cpp
@@ -1,1 +1,29 @@
-//
+#include<iostream>
+using namespace std;
+class student
+{
+int roll_no;
+char name[30];
+public:
+void get_data();
+void put_data();
+};
+void student :: get_data()
+{
+cout<<"enter student roll number : ";
+cin>>roll_no;
+cout<<"enter student name : "<<endl;
+cin>>name;
+}
+void student :: put_data()
+{
+cout<<"student roll number: "<<rollno;
+cout<<'student name : " <<name<<endl;
+}
+int main ()
+{
+student s1;
+s1.get_data();
+s1.put_data();
+return 0;
+}
